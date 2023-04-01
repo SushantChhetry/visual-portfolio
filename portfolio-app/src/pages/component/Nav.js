02795e93938efd955f994a46c93d03f4d3a31292
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-04-01 14:48:55 -0500

    Add: basic page structure

--- portfolio-app/src/pages/component/Nav.js
+++ portfolio-app/src/pages/component/Nav.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Nav
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="nav-wrapper">
+      <nav className="nav">
+        <h1>SUSHANT</h1>
+        <span>
+          <button onClick={toggleMenu}>Click Me</button>
+          {isOpen ? (
+            <ul className="links">
+              <li className="navbtn">UP</li>
+              <li className="navbtn">CONTACT</li>
+              <li className="navbtn">RESUME</li>
+            </ul>
+          ) : (
+            ""
+          )}
+        </span>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
