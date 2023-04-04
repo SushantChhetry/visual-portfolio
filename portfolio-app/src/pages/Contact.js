7c53cd15fa70fcffac345ab6eec1b7533adf6471
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-04-04 12:55:30 -0500

    Added Basic CSS to form

--- portfolio-app/src/pages/Contact.js
+++ portfolio-app/src/pages/Contact.js
@@ -5,9 +5,14 @@ const Contact = () => {
     <div className="contact-wrapper">
       <h1>CONTACT ME</h1>
       <form>
-        <input type="text" placeholder="first name" />
+        <label for="firstname">First Name:</label>
+        <input type="text" placeholder="first name" required />
+        <label for="lastname">Last Name:</label>
         <input type="text" placeholder="last name" />
-        <input type="textarea" />
+        <label for="email">Email:</label>
+        <input type="email" placeholder="email" required />
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>{" "}
         <input type="submit" />
       </form>
     </div>
--- portfolio-app/src/pages/intro.js
+++ portfolio-app/src/pages/intro.js
@@ -1,4 +1,7 @@
 import { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 const intro = () => {
   const pRef = useRef(null);
@@ -11,7 +14,8 @@ const intro = () => {
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          const sentence = "Hello, world! This is a typing paragraph.";
+          const sentence =
+            "I'm a frontend developer with expertise in React JS, passionate about crafting visually appealing and functional websites that provide great user experience. My portfolio showcases some of my best works, which are designed and developed with attention to detail and latest industry trends.";
           let index = 0;
           const intervalId = setInterval(() => {
             const currentText = pRef.current.textContent;
@@ -40,6 +44,7 @@ const intro = () => {
       <div className="intro">
         <p ref={pRef}></p>
       </div>
+      <FontAwesomeIcon icon={faReact} size="lg" />
     </div>
   );
 };
