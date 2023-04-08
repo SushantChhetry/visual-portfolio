922df8fd6c77dbbeeb20c31f1b4b8ee3949bceda
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-04-07 19:31:23 -0500

    Add: animation to the model

--- portfolio-app/src/pages/component/Box.js
+++ portfolio-app/src/pages/component/Box.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const Box = (props) => {
+  const myMesh = useRef();
+  useFrame(({ clock }) => {
+    myMesh.current.rotation.x = clock.getElapsedTime() / 2.5;
+  });
+
   return (
-    <mesh {...props} recieveShadow={true} castShadow>
+    <mesh {...props} recieveShadow={true} castShadow ref={myMesh}>
       <boxBufferGeometry />
       <meshPhysicalMaterial color={"white"} />
     </mesh>
